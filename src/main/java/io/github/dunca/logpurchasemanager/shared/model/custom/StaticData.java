6992e06322583909aa6ff129a9e51e46849b3a84
dunca <flv@mail.com>
2018-04-20 10:05:54 +0300

    FullDb: renamed to StaticData

--- src/main/java/io/github/dunca/logpurchasemanager/shared/model/custom/StaticData.java
+++ src/main/java/io/github/dunca/logpurchasemanager/shared/model/custom/StaticData.java
@@ -1,9 +1,6 @@
 package io.github.dunca.logpurchasemanager.shared.model.custom;
 
 import io.github.dunca.logpurchasemanager.shared.model.Acquirer;
-import io.github.dunca.logpurchasemanager.shared.model.Acquisition;
-import io.github.dunca.logpurchasemanager.shared.model.AcquisitionItem;
-import io.github.dunca.logpurchasemanager.shared.model.LogPrice;
 import io.github.dunca.logpurchasemanager.shared.model.LogQualityClass;
 import io.github.dunca.logpurchasemanager.shared.model.Supplier;
 import io.github.dunca.logpurchasemanager.shared.model.TreeSpecies;
@@ -14,14 +11,21 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class FullDb {
+public class StaticData {
     private List<Acquirer> acquirers;
-    private List<Acquisition> acquistions;
-    private List<AcquisitionItem> acquisitionItems;
-    private List<LogPrice> logPrices;
     private List<LogQualityClass> logQualityClasses;
     private List<Supplier> suppliers;
     private List<TreeSpecies> treeSpecies;
     private List<WoodCertification> woodCertifications;
     private List<WoodRegion> woodRegions;
+
+    public StaticData(List<Acquirer> acquirers, List<LogQualityClass> logQualityClasses, List<Supplier> suppliers,
+                      List<TreeSpecies> treeSpecies, List<WoodCertification> woodCertifications, List<WoodRegion> woodRegions) {
+        this.acquirers = acquirers;
+        this.logQualityClasses = logQualityClasses;
+        this.suppliers = suppliers;
+        this.treeSpecies = treeSpecies;
+        this.woodCertifications = woodCertifications;
+        this.woodRegions = woodRegions;
+    }
 }
